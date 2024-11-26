5591385fe133a7581a8e82bb0d96547fcf327a35
Dawa <atayewdawlet@gmail.com>
2024-11-26 23:45:14 +0500

    add animation to info list-but not finally

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,32 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import './info.scss'
+import "./info.scss";
 
-
-function Info(){
-    const [ currentItem, setCurrentItem ] = useState(0);
+function Info() {
+    const [currentItem, setCurrentItem] = useState(0);
     const [prevItems, setPrevItems] = useState([]);
     const [isScrolling, setIsScrolling] = useState(false);
 
-    const infoItems = ['Аналитика рынка и аудит бизнеса', 'бизнес-стратегия', 'брендинговая стратегия']
+    const infoItems = [
+        {
+            title: "Аналитика рынка и аудит бизнеса",
+            text: "Погружение в мир вашего бизнеса начинается с детального анализа ключевых показателей эффективности и комплексного изучения бизнес-процессов. Мы проводим глубокую оценку данных, выявляем ключевые тенденции рынка, конкурентного окружения и потребительских предпочтений. Сотрудничая с ведущими отраслевыми экспертами, мы предоставляем обоснованные рекомендации и формируем персонализированные решения, которые учитывают как внутренние ресурсы компании, так и внешние рыночные факторы.",
+            list: [
+                "анализ рынка и конкурентов",
+                "аудит показателей бизнеса и операционных решений",
+                "custdev и привлечение отраслевых экспертов",
+                "формирование решений на основе фактических срезов внешнего окружения",
+            ],
+        },
+        {
+            title: "Бизнес-стратегия",
+            text: "Погружение в мир вашего бизнеса начинается с детального анализа ключевых показателей эффективности и комплексного изучения бизнес-процессов. Мы проводим глубокую оценку данных, выявляем ключевые тенденции рынка, конкурентного окружения и потребительских предпочтений. Сотрудничая с ведущими отраслевыми экспертами, мы предоставляем обоснованные рекомендации и формируем персонализированные решения, которые учитывают как внутренние ресурсы компании, так и внешние рыночные факторы.",
+            list: [
+                "анализ рынка и конкурентов",
+                "аудит показателей бизнеса и операционных решений",
+                "custdev и привлечение отраслевых экспертов",
+                "формирование решений на основе фактических срезов внешнего окружения",
+            ],
+        },
+        {
+            title: "Брендинговая стратегия",
+            text: "Погружение в мир вашего бизнеса начинается с детального анализа ключевых показателей эффективности и комплексного изучения бизнес-процессов. Мы проводим глубокую оценку данных, выявляем ключевые тенденции рынка, конкурентного окружения и потребительских предпочтений. Сотрудничая с ведущими отраслевыми экспертами, мы предоставляем обоснованные рекомендации и формируем персонализированные решения, которые учитывают как внутренние ресурсы компании, так и внешние рыночные факторы.",
+            list: [
+                 "анализ рынка и конкурентов",
+                "аудит показателей бизнеса и операционных решений",
+                "custdev и привлечение отраслевых экспертов",
+                "формирование решений на основе фактических срезов внешнего окружения",
+            ],
+        },
+    ];
 
     useEffect(() => {
         const handleScroll = (e) => {
             e.stopPropagation();
             e.preventDefault();
 
-            if (isScrolling) return; // Блокировка повторного вызова
+            if (isScrolling) return;
 
-            const scrollThreshold = 50; // Порог чувствительности к скроллу
+            const scrollThreshold = 50; // Порог чувствительности
             if (e.deltaY > scrollThreshold && currentItem < infoItems.length - 1) {
-                // Переход вниз
                 setPrevItems((prev) =>
                     prev.includes(currentItem) ? prev : [...prev, currentItem]
                 );
                 setCurrentItem((prev) => prev + 1);
                 setIsScrolling(true);
             } else if (e.deltaY < -scrollThreshold && currentItem > 0) {
-                // Переход вверх
                 setPrevItems((prev) =>
                     prev.filter((item) => item !== currentItem - 1)
                 );
@@ -34,7 +62,7 @@ function Info(){
                 setIsScrolling(true);
             }
 
-            setTimeout(() => setIsScrolling(false), 500); // Сброс блокировки через 500 мс
+            setTimeout(() => setIsScrolling(false), 500);
         };
 
         const infoMiddle = document.querySelector(".info__middle");
@@ -43,13 +71,14 @@ function Info(){
         return () => infoMiddle.removeEventListener("wheel", handleScroll);
     }, [currentItem, infoItems.length, isScrolling]);
 
-
-    return(
+    return (
         <section className="info">
             <div className="container">
                 <div className="info__wrapper">
                     <div className="info__left">
-                        <div className="info__current"><span>{currentItem + 1}</span>/7</div>
+                        <div className="info__current">
+                            <span>{currentItem + 1}</span>/{infoItems.length}
+                        </div>
                         <div className="info__link">Посмотреть кейсы</div>
                     </div>
                     <div className="info__middle">
@@ -59,30 +88,42 @@ function Info(){
                                     key={index}
                                     className={`info__item ${
                                         index === currentItem
-                                            ? 'active'
+                                            ? "active"
                                             : prevItems.includes(index)
-                                            ? 'previous'
-                                            : 'inactive'
+                                            ? "previous"
+                                            : "inactive"
                                     }`}
                                 >
-                                    {item}
+                                    {item.title}
                                 </div>
                             ))}
                         </div>
                     </div>
                     <div className="info__right">
-                        <div className="info__text">Погружение в мир вашего бизнеса начинается с детального анализа ключевых показателей эффективности и комплексного изучения бизнес-процессов. Мы проводим глубокую оценку данных, выявляем ключевые тенденции рынка, конкурентного окружения и потребительских предпочтений. Сотрудничая с ведущими отраслевыми экспертами, мы предоставляем обоснованные рекомендации и формируем персонализированные решения, которые учитывают как внутренние ресурсы компании, так и внешние рыночные факторы.</div>
-                        <ul className="info__list">
-                            <li className="info__list_item">анализ рынка и конкурентов</li>
-                            <li className="info__list_item">аудит показателей бизнеса и операционных решений</li>
-                            <li className="info__list_item">custdev и привлечение отраслевых экспертов</li>
-                            <li className="info__list_item">формирование решений на основе фактических срезов внешнего окружения</li>
-                        </ul>
+                        {infoItems.map((item, index) => (
+                            <div
+                                key={index}
+                                className={`info__right_item ${
+                                    index === currentItem
+                                        ? "fade-in-up"
+                                        : "fade-out-down"
+                                }`}
+                            >
+                                <div className="info__text">{item.text}</div>
+                                <ul className="info__list">
+                                    {item.list.map((listItem, idx) => (
+                                        <li key={idx} className="info__list_item">
+                                            {listItem}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default Info
+export default Info;
